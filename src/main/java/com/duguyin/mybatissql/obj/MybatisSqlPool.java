d9f2f5ab6a6ab6192aa7f203dd6601ca486b03c2
duguyin <liuyindog@163.com>
2018-10-10 23:11:37 +0800

    [dev] 4 df3

--- src/main/java/com/duguyin/mybatissql/obj/MybatisSqlPool.java
+++ src/main/java/com/duguyin/mybatissql/obj/MybatisSqlPool.java
@@ -1,6 +1,8 @@
 package com.duguyin.mybatissql.obj;
 
 import com.duguyin.mybatissql.enums.LogicOperator;
+import com.duguyin.mybatissql.exceptions.ParseException;
+import com.duguyin.mybatissql.tool.StringTool;
 
 import java.util.*;
 
@@ -59,6 +61,8 @@ public class MybatisSqlPool<T> {
         OP op;
 
         List<String> columns = new ArrayList<>();
+        int insertRepeatCount = 1;
+
 
         List<String> values;
         List<String> countColumns = new ArrayList<>();
@@ -107,14 +111,99 @@ public class MybatisSqlPool<T> {
             return this;
         }
 
-        public MybatisSQL limit(int begin, int offset){
+        public MybatisSQL LIMIT(int begin, int offset){
             this.limit[0] = begin;
             this.limit[1] = offset;
             return this;
         }
 
-        public MybatisSQL count(String property){
+        public MybatisSQL COUNT(String property){
+            Objects.requireNonNull(property, "count property in null");
+            this.countColumns.add(property);
+            return this;
+        }
 
+        public String getSql(){
+            if(StringTool.isNullOrEmpty(tableName)){
+                throw new ParseException("table name is null or empty");
+            }
+            StringBuilder sql = new StringBuilder();
+            sql.append(op.getOp() + " ");
+            if(OP.INSERT == op){
+                sql.append(tableName);
+                sql.append(getInsertColumns(columns)).append(" values ").append(getMybatisInsertValueColumns(columns, insertRepeatCount));
+            }
+            else if(OP.UPDATE == op){
+                sql.append(tableName).append(" SET ");
+                sql.append(getUpdateSqlFragment(columns));
+            }
+
+
+
+            return sql.toString();
+        }
+
+        private String getUpdateSqlFragment(List<String> properties) {
+            Objects.requireNonNull(properties, "update properties is null");
+            int size = properties.size();
+            if(size == 0){
+                throw  new ParseException("update properties is empty");
+            }
+            // TODO
+
+
+            return null;
+        }
+
+        private String getInsertColumns(List<String> properties){
+            Objects.requireNonNull(properties, "insert properties is null");
+            int size = properties.size();
+            if(size == 0){
+                throw  new ParseException("insert properties is empty");
+            }
+            Map<String, PropertyColumnMapping> defaultMappingMap = mapping.getDefaultMappingMap();
+            StringBuilder insertColumnsBuilder = new StringBuilder();
+            insertColumnsBuilder.append(" (");
+            for(int i = 0; i< size; i++){
+                String property = properties.get(i);
+                if(Objects.isNull(defaultMappingMap)){
+                    insertColumnsBuilder.append(property);
+                }else{
+                    PropertyColumnMapping pcm = defaultMappingMap.get(property);
+                    insertColumnsBuilder.append(Objects.isNull(pcm) ? property : pcm.getColumn());
+                }
+                if(i < size -1){
+                    insertColumnsBuilder.append(") ");
+                }
+            }
+            return insertColumnsBuilder.toString();
+        }
+
+
+        private String getMybatisInsertValueColumns(List<String> properties, int repeatCount){
+            Objects.requireNonNull(properties, "insert properties is null");
+            int size = properties.size();
+            if(size == 0){
+                throw new ParseException("insert properties is empty");
+            }
+            if(repeatCount < 1){
+                throw new ParseException("repeat count must gte 1");
+            }
+            StringBuilder values = new StringBuilder();
+            for(int i = 0; i < repeatCount; i ++){
+                values.append(" ( ");
+                for(int j = 0; j < size; j ++){
+                    values.append("#{").append(properties.get(j)).append("}");
+                    if(j < size -1){
+                        values.append(", ");
+                    }
+                }
+                values.append(" ) ");
+                if(i < repeatCount - 1){
+                    values.append(", ");
+                }
+            }
+            return values.toString();
         }
 
     }
