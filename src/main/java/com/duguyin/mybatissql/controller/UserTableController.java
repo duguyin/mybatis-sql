d80e563f0c3746d5b3359f1bb11378b9da5c5600
duguyin <liuyindog@163.com>
2018-11-20 22:10:49 +0800

    [dev] 4 df33

--- src/main/java/com/duguyin/mybatissql/controller/UserTableController.java
+++ src/main/java/com/duguyin/mybatissql/controller/UserTableController.java
@@ -7,6 +7,7 @@ import com.duguyin.mybatissql.obj.MybatisSqlPool;
 import com.duguyin.mybatissql.service.UserTableService;
 import com.duguyin.mybatissql.service.impl.UserTableServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/tt")
 public class UserTableController {
+
+    @Value("${ttt.ok}")
+    private String tttOK;
 
     @Autowired
     private UserTableService service;
